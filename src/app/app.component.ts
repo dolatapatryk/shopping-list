e46a520b705f693d782efb5782e0d0bc4259454d
dolatapatryk <dolata.patryk1@gmail.com>
2021-03-10 17:08:35 +0100

    fixed safari mobile problem with 100vh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'shopping-list-app';
+
+    constructor() {
+        this.setViewportHeightProperty();
+
+        window.addEventListener('resize', () => this.setViewportHeightProperty());
+    }
+
+    private setViewportHeightProperty() {
+        const vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
 }
