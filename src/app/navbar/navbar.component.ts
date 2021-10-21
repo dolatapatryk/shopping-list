7740d3a39e8dbdb6f27f7e6bf998694a2aad5448
Patryk Dolata <patrykdolata@MacBook-Air-Patryk.local>
2021-10-21 21:00:09 +0200

    Close sidebar after click on navbar item and navigate

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,25 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 
+export interface NavbarItem {
+    routerLink: string;
+    itemName: string;
+}
+
 @Component({
     selector: 'app-navbar',
     templateUrl: './navbar.component.html',
     styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+    items: NavbarItem[] = [
+        {routerLink: '/shopping', itemName: 'Zakupy'},
+        {routerLink: '/departments', itemName: 'Działy'},
+        {routerLink: '/products', itemName: 'Produkty'}
+    ];
+
     @Output()
     toggleSidenav: EventEmitter<any> = new EventEmitter<any>();
+
+    @Output()
+    closeSidenav: EventEmitter<any> = new EventEmitter<any>();
 }
