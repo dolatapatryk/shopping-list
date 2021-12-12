8bb928a1b43181707d2210ac075a268d3c5db090
dolatapatryk <dolata.patryk1@gmail.com>
2021-12-12 16:49:49 +0100

    mark items in shopping list

--- src/app/shopping/shopping-list/shopping-list.component.ts
+++ src/app/shopping/shopping-list/shopping-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ShoppingList } from '../../models/shopping-list';
 import { ActivatedRoute } from '@angular/router';
+import { ShoppingListProduct } from '../../models/product';
 
 @Component({
     selector: 'app-shopping-list',
@@ -9,15 +10,25 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ShoppingListComponent implements OnInit {
     list: ShoppingList;
+    checkedCount = 0;
+    allChecked = false;
 
     constructor(private route: ActivatedRoute) {
     }
 
     ngOnInit(): void {
         this.list = this.route.snapshot.data.list;
-        console.log(':::list', this.list);
+        this.checkedCount = this.list.products.filter(product => product.mark).length;
+        this.checkAllMarked();
     }
 
-    markItemChange(item: any, checked: boolean) {
+    markItemChange(item: ShoppingListProduct, checked: boolean) {
+        item.mark = checked;
+        this.checkedCount += checked ? 1 : -1;
+        this.checkAllMarked();
+    }
+
+    private checkAllMarked() {
+        this.allChecked = this.checkedCount === this.list.products.length;
     }
 }
